7b420725a5c0c01e53bff9d80a9a705717b5e675
Yvette Cook <yvettecook@gmail.com>
2014-09-20 18:04:16 +0100

    Coach can hold passangers

--- lib/coach.rb
+++ lib/coach.rb
@@ -1,12 +1,20 @@
 class Coach
 
 	def initialize
-			@capacity = 40
+		@capacity = 40
+		@on_board = []
 	end
 
 	def capacity
 		@capacity 
 	end
 
+	def add_passangers(passangers)
+		@on_board << passangers
+	end
+
+	def passanger_count
+		@on_board.length
+	end
 
 end
--- spec/coach_spec.rb
+++ spec/coach_spec.rb
@@ -9,4 +9,10 @@ describe Coach do
 		expect(coach.capacity).to eq(40)
 	end
 
+	it "should be able to hold people" do
+		coach.add_passangers(:person)
+		expect(coach.passanger_count).to eq(1)
+	end
+
+
 end
