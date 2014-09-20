97845b9b47728211a32cd65767b3100ecbed3620
Yvette Cook <yvettecook@gmail.com>
2014-09-20 20:48:54 +0100

    Person double removed from station spec

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,14 +1,15 @@
 require 'station'
 require 'train'
+require 'person'
 
 describe Station do 
 
 	let (:station) { Station.new }
-	let (:person) { double :person }
+	let (:person) { Person.new }
 	let (:train) { Train.new }
 
 	it "should hold passangers" do
-		station.add(:person)
+		station.add(person)
 		expect(station.passangers_count).to eq(1)
 	end
 
