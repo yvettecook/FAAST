b4a69e1e3301d9aeb43b2ed08c4d90a594eebaa3
Yvette Cook <yvettecook@gmail.com>
2014-09-20 20:12:58 +0100

    Checked train can arrive at station

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -5,9 +5,15 @@ describe Train do
 
 	let (:train) { Train.new }
 	let (:coach) { Coach.new }
+	let (:station) { Station.new }
 
 	it "should be created with a default of 3 coaches " do
 		expect(train.coaches.length).to eq(3)
+	end
+
+	it "should be able to arrive at a station" do
+		train.arrive(station)
+		expect(station.train_count).to eq(1)
 	end
 	
 end
