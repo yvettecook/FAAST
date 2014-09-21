d1d2a927125641557979e0daaab80db09f3d1cfb
Yvette Cook <yvettecook@gmail.com>
2014-09-21 20:09:51 +0100

    Person cannot enter station if balance is less than 2

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,25 @@
 class Person
 
 	def initialize
-		@balance = rand(20)
+		@balance = 10
 	end
 
 	def balance
 		@balance
 	end
 
+	def set_balance(balance)
+		@balance = balance
+	end
+
+
 	def touch_in(station)
-		station.add(self)
-		@balance = @balance -2
+		if @balance > 2
+			station.add(self)
+			@balance = @balance -2
+		else
+			raise "Insufficent balance to travel"
+		end
 	end
 
 	def touch_out(station)
