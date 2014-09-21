af6417446b211b0dbfcb89cc8990ea9bf0b02790
Yvette Cook <yvettecook@gmail.com>
2014-09-21 12:20:28 +0100

    Issues log added

--- lib/train.rb
+++ lib/train.rb
@@ -38,3 +38,40 @@ class Train
 
 end
 
+class Coach
+
+	def initialize
+		@capacity = 40
+		@on_board = []
+	end
+
+	def capacity
+		@capacity 
+	end
+
+	def add_passager(passager)
+		@on_board << passager
+	end
+
+	def remove(passager)
+		@on_board.pop
+	end
+
+	def passagers_count
+		@on_board.length
+	end
+
+	def full?
+		passagers_count == @capacity
+	end
+
+	def empty(station)
+		passagers_count.times do 
+			@on_board.each do |person|
+				station.add(remove(person))
+			end		
+		end
+	end
+
+end
+
